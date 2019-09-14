953d66bf96297954509a617d1ec4b7bdb8389924
FangFeiyue <18766131540@163.com>
2019-09-14 08:42:59 +0800

    更新文件

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <el-header>
+     <my-header></my-header>
+     header
+    </el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+<script>
+import MyHeader from '@/components/public/header/index';
+export default {
+  components: {
+    MyHeader
+  }
+}
+</script>
+<style>
+</style>
